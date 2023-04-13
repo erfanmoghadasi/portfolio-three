79b0241b9271da92a4cd06df8a88c438d2401481
erfan moghadasi <erfan.moqadasii@gmail.com>
2023-04-13 03:32:08 +0330

    fix: add vite config and rollup option 2

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ export default {
     build: {
       rollupOptions: {
         external: [
-          'THREE/examples/jsm/loaders/GLTFLoader.js'
+          '/THREE/examples/jsm/loaders/GLTFLoader.js'
         ]
       }
     }
